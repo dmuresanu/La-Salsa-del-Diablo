9d927e9214830d63a9a296de8d8d0160b385e742
Doru Muresanu <doru.muresanu@yahoo.ie>
2024-12-09 01:07:18 +0000

    Define Event and EventBooking models

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bxdu6n8t=fgg22&1w%r2c2k5-t+-jz=jc)em_zgpbhrr5&gddu'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
